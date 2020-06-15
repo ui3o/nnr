5a25b129fc4eb30430230ee8b7792578dfc6b2d4
ui3o.com <ui3o.com@gmail.com>
2020-06-15 19:49:26 +0200

    default feature extend

--- README.md
+++ README.md
@@ -3,3 +3,43 @@
 **N**ode based **N**pm **R**un
 
 Easy replacement for `npm run`.
+
+# Install 
+* ```npm i -g nnr```
+* Install [Cygwin](https://www.cygwin.com/) on Windows! see [here](#Dependencies)
+
+# What is it resolve?
+* cross platform (windows and linux) script in npm-script
+* resolves
+    * environment variable, possible to use only $ENV_VAR, no longer need on %ENV_VAR% on window
+    * possible to use same command on windows and linux with [Cygwin](https://www.cygwin.com/)
+
+
+# Features
+
+* `comment:` tag for all script
+* run multiple npm-scripts in parallel or sequential like *[npm-run-all](https://github.com/mysticatea/npm-run-all)*
+* all script runs inside a *bash* shell
+
+# Examples
+
+Examples are located in [test/package.json](test/package.json) file.
+
+# Dependencies
+
+* Install [Cygwin](https://www.cygwin.com/)
+* **IMPORTANT!! Add** Cygwin **path** to **Environment** variables to **top** level, which replace the default windows commands like find and etc.
+
+![Alt text](/docs/windows_settings.png?raw=true)
+
+# Usage
+
+| command        | description|
+| ------------- |:-------------|
+| *nrr* | without parameters it gives a nice *choices* menu |
+| *nrr --help* | gives a standard help manual which is generated with [yargs](https://www.npmjs.com/package/yargs)|
+
+
+# Alternatives
+
+* [npm-run-all](https://github.com/mysticatea/npm-run-all)
--- index.js
+++ index.js
@@ -12,30 +12,39 @@ const COMMENT = 'comment:'
 const options = yargs
     .usage(`Name: Node based Npm Run, Easy replacement for npm run.\n\nUsage: nnr [OPTION...] scripts.key`)
     //.option("n", { alias: "name", describe: "Your name", type: "string", demandOption: true })
+    .option("j", { alias: "packagejsonpath", describe: "Path to package.json file", type: "string" })
     .option("p", { alias: "parallel", describe: "Run a group of tasks in parallel", type: "boolean" })
     .option("s", { alias: "sequential", describe: "Run a group of tasks sequentially", type: "string" })
     .alias('v', 'version')
     .alias('h', 'help')
-    .epilog('copyright 2020')
+    .epilog('copyright@2020')
     .argv;
 
-const scripts = JSON.parse(fs.readFileSync('package.json')).scripts;
+console.log(JSON.stringify(options));
+const currentScriptId = options._[0];
+const packagejson = options.j ? options.j : 'package.json';
+const scripts = JSON.parse(fs.readFileSync(packagejson)).scripts;
 const choices = [];
-Object.keys(scripts).forEach(key => {
-    if (!key.startsWith(COMMENT)) {
-        const mitem = { title: key, value: key };
-        // find comment
-        Object.keys(scripts).forEach(comment => {
-            if (comment.startsWith(COMMENT)) {
-                const commentKey = comment.replace(COMMENT, '');
-                if (commentKey === key) {
-                    mitem.description = scripts[comment];
+
+if(currentScriptId) {
+
+} else {
+    Object.keys(scripts).forEach(key => {
+        if (!key.startsWith(COMMENT)) {
+            const mitem = { title: key, value: key };
+            // find comment
+            Object.keys(scripts).forEach(comment => {
+                if (comment.startsWith(COMMENT)) {
+                    const commentKey = comment.replace(COMMENT, '');
+                    if (commentKey === key) {
+                        mitem.description = scripts[comment];
+                    }
                 }
-            }
-        });
-        choices.push(mitem);
-    }
-});
+            });
+            choices.push(mitem);
+        }
+    });
+}
 
 
 (async function () {
@@ -49,7 +58,6 @@ Object.keys(scripts).forEach(key => {
         await runcmd(scripts[response.value]);
     }
 })();
-
 
 async function runcmd(script, options) {
     options = !options ? {} : options;
